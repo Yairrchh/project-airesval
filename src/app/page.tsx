41d7d877542888870c09b2e27f0c417c1dc65afd
yairrchh <chirayair@gmail.com>
2024-08-09 09:35:25 -0400

    adding dockerfile production

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,17 @@
-import styles from "./page.module.css";
-import './globals.css'
 
+import { redirect } from 'next/navigation';
+import { getServerSession } from 'next-auth/next';
+import { authOptions } from '../app/api/auth/[...nextauth]/route'; // Asegúrate de importar correctamente
 
-export default function Home() {
+export default async function Home() {
+  const session = await getServerSession(authOptions);
 
-  return (
-    <main className={styles.main}>
-      <div>Hola sd</div>
-    </main>
-  );
+  if (!session) {
+    // Redirige a la página de inicio de sesión si no hay sesión
+    redirect('/auth/login');
+  }
+
+  // Si hay una sesión, puedes renderizar el contenido de la página principal
+  return <h1>Bienvenido a la aplicación</h1>;
 }
+
