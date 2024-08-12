9767586c5954d92649de0de6a71d70c64967424a
yairrchh <chirayair@gmail.com>
2024-08-12 10:08:28 -0400

    feat route

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,15 @@
 
 import { redirect } from 'next/navigation';
 import { getServerSession } from 'next-auth/next';
-import { authOptions } from '../app/api/auth/[...nextauth]/route'; // Asegúrate de importar correctamente
+//import { authOptions } from '../app/api/auth/[...nextauth]/route'; // Asegúrate de importar correctamente
 
 export default async function Home() {
-  const session = await getServerSession(authOptions);
+  // const session = await getServerSession(authOptions);
 
-  if (!session) {
-    // Redirige a la página de inicio de sesión si no hay sesión
-    redirect('/auth/login');
-  }
+  // if (!session) {
+  //   // Redirige a la página de inicio de sesión si no hay sesión
+  //   redirect('/auth/login');
+  // }
 
   // Si hay una sesión, puedes renderizar el contenido de la página principal
   return <h1>Bienvenido a la aplicación</h1>;
